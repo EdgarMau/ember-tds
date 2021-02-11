1c435b5432d33aca0c41e99266ad3e4ba9121bfd
Edgar MAURANNE <hiroedg@gmail.com>
2021-02-11 09:11:19 +0100

    commit

---
+++ app/classes/Services.js
@@ -0,0 +1,45 @@
+import { computed } from '@ember/object';
+export default class Services {
+  services = [];
+  test=0;
+
+  constructor(services, promo) {
+    this.services = services;
+    this.promos = promo;
+  }
+
+  @computed('services.@each.active')
+
+  get countActive(){
+    return this.services.filterBy('active',true).length;
+
+  }
+
+
+  get sumActive(){
+    var total = 0;
+    this.services.filterBy('active',true).forEach(function(service) {
+      total += service.price;
+    });
+    return total;
+  }
+
+  get coefPromo(){
+    for (const [key, value] of Object.entries(this.promos)) {
+      //console.log(`${key}: ${value}`);
+      console.log(key);
+      if (key == this.enterValue){
+        return value;
+      }
+    }
+    return 0;
+  }
+
+  get remise(){
+    return this.sumActive * this.coefPromo;
+  }
+
+  get totalAvecRemise() {
+    return this.sumActive - this.remise;
+  }
+}
---
+++ app/routes/ex2.js
@@ -0,0 +1,28 @@
+import Route from '@ember/routing/route';
+import Services from '../classes/Services';
+import { datas } from '../data/data';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class Ex2Route extends Route {
+  @tracked nbrActive = 0;
+
+  model() {
+    return new Services(datas);
+  }
+
+  @action
+  toggleActive(service) {
+    if (service.active) {
+      service.active = false;
+    } else {
+      service.active = true;
+    }
+    console.log(service);
+    this.nbrActive = this.model.countActive;
+  }
+  @action
+  valider() {
+    this.transitionTo('/ex2/promo');
+  }
+}
